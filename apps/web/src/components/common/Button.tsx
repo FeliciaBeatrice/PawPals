89d7ab187f003a1ae5c62e50585535424f1cd14b
Lish <feliciabeatrice7@gmail.com>
2025-03-23 21:31:26 +0800

    route button component

--- apps/web/src/components/common/Button.tsx
+++ apps/web/src/components/common/Button.tsx
@@ -1,18 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Button({
   text,
   onClick,
   className = "",
+  href,
 }: {
   text: string;
   onClick?: () => void;
   className?: string;
+  href?: string;
 }) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (href) {
+      router.push(href);
+    }
+
+    if (onClick) {
+      onClick();
+    }
+  }
   return (
     <button
       className={`px-4 py-2 border-4 border-secondary-700 bg-white text-secondary-900 font-montserrat font-bold rounded-full hover:bg-primary-700 hover:text-white transition ${className}`}
-      onClick={onClick}>
+      onClick={handleClick}>
         {text}
     </button>
   );
--- apps/web/src/components/home/Header.tsx
+++ apps/web/src/components/home/Header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
       <header className="flex items-center justify-between bg-secondary-900 p-2 px-6">
         <img src="./designs/logo.png" alt="Logo" className="h-20" />
         <div className="flex items-center space-x-4 pr-4">
-          <Button text="Get Started" onClick={() => {}} />
-          <Button text="Log In" onClick={() => {}} />
+          <Button text="Get Started" onClick={() => {}} href="/onboarding" />
+          <Button text="Log In" onClick={() => {}} href="/onboarding" />
         </div>
       </header>
     );
